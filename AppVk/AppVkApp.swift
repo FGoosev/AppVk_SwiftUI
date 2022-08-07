22f885899b6b49f7534594f12921c4985700a52e
FGoosev <FGoosev@gmail.com>
2022-08-07 22:20:31 +0400

    Initial Commit

---
+++ AppVk/AppVkApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppVkApp.swift
+//  AppVk
+//
+//  Created by Alexandr Gusev on 07.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppVkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
